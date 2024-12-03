4c0ab3f938327f32a9302f909299bba8e5f0a489
Thabo Krick <thabokrick@gmail.com>
2024-12-03 15:14:02 +0100

    added Back-to-Back test MIL vs. SIL #test

--- scripts/run_tests.py
+++ scripts/run_tests.py
@@ -26,9 +26,21 @@ rbt_exec_payload = {
     'UIDs': scope_uids,
    'data' : { 'execConfigNames' : [ 'SL MIL', 'SIL' ] }
 }
-response = ep.post('scopes/test-execution-rbt', rbt_exec_payload, message="Executing requirements-based tests")
-util.print_rbt_results(response)
+rbt_response = ep.post('scopes/test-execution-rbt', rbt_exec_payload, message="Running requirements-based tests")
+util.print_rbt_results(rbt_response)
 test_cases = ep.get('test-cases-rbt')
+
+# B2B test (with automatic test generation for full MCDC coverage)
+vector_gen_settings = {
+    'scopeUid'  : toplevel_scope_uid,
+    'pllString' : 'MCDC' 
+}
+ep.post('coverage-generation', vector_gen_settings, message="Generating vectors")
+b2b_coverage = ep.get(f"scopes/{toplevel_scope_uid}/coverage-results-b2b")
+
+# B2B MIL vs. SIL execution
+b2b_response = ep.post(f"scopes/{toplevel_scope_uid}/b2b", { 'refMode': 'SL MIL', 'compMode': 'SIL' }, message="Running B2B test")
+util.print_b2b_results(b2b_response, b2b_coverage)
 
 # Create project report
 report = ep.post(f"scopes/{toplevel_scope_uid}/project-report", message="Creating test report")
@@ -37,7 +49,8 @@ report_dir = os.path.abspath('reports')
 ep.post(f"reports/{report['uid']}", { 'exportPath': report_dir, 'newName': 'report' })
 # Dump JUnit XML report
 util.dump_testresults_junitxml(
-    rbt_results=response,
+    b2b_result=b2b_response,
+    rbt_results=rbt_response,
     scopes=scopes,
     test_cases=test_cases,
     start_time=exec_start_time,
